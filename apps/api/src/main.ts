48eaa5fe9512d2941f66c744f9077e78a1eec90a
myltx <summmayl@gmail.com>
2025-01-02 11:15:56 +0800

    fix: Enhance CORS configuration for improved flexibility and maintainability
    - Updated CORS settings in main.ts to allow all origins with specified methods and credentials, enhancing cross-origin request handling.
    - Cleaned up commented-out code related to manual CORS header settings for better readability and maintainability.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(`请访问：http://localhost:${process.env.PORT ?? 8081}`);
   // 启用跨域
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  // 手动设置跨域头部
+  // app.use((req: any, res: any, next: Function) => {
+  //   res.header('Access-Control-Allow-Origin', 'https://nav.myltx.top');
+  //   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  //   res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+  //   res.header('Access-Control-Allow-Credentials', 'true');
+  //   next();
+  // });
   await app.listen(process.env.PORT ?? 8081);
 }
 bootstrap();
