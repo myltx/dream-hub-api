bc125a9d2dff8a243a1f1f29366fb0b914343196
myltx <summmayl@gmail.com>
2025-01-02 10:13:17 +0800

    fix: Update CORS configuration to allow all origins and simplify settings
    - Changed CORS origin setting to allow all origins by using a wildcard '*', enhancing flexibility for cross-origin requests.
    - Removed the previous origin validation logic to streamline the CORS setup.
    - Updated allowedHeaders to include 'Authorization' and 'content-type' for better request handling.
    - Commented out the credentials option for clarity, maintaining focus on essential CORS settings.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,16 +8,18 @@ async function bootstrap() {
   //  [process.env.REQUEST_ORIGIN ?? 'http://localhost:3000']
   const allowedOrigins = process.env.REQUEST_ORIGIN?.split(',') || ['https://nav.myltx.top']
   app.enableCors({
-    origin: (origin, callback) => {
-      // 检查是否在允许列表中
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }, // 允许的前端域名
+    // origin: (origin, callback) => {
+    //   // 检查是否在允许列表中
+    //   if (!origin || allowedOrigins.includes(origin)) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error('Not allowed by CORS'));
+    //   }
+    // }, // 允许的前端域名
+    origin: "*",
+      allowedHeaders: ['Authorization', 'content-type'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',              // 允许的 HTTP 方法
-    credentials: true,                                      // 是否允许携带凭证（如 Cookies）
+    // credentials: true,                                      // 是否允许携带凭证（如 Cookies）
   });
   await app.listen(process.env.PORT ?? 8081);
 }
