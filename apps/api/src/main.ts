e3a61affdf8d3bbb8b5150356d902cf0c114dd84
myltx <summmayl@gmail.com>
2025-01-02 09:50:25 +0800

    fix: Refine CORS configuration to validate origins and enhance security
    - Updated CORS settings to check if the request origin is in the allowedOrigins list, providing a more secure approach to handling cross-origin requests.
    - Changed the default allowed origin to 'https://nav.myltx.top' for improved specificity.
    - Removed the commented-out logging for allowed frontend domains to clean up the code.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,11 +5,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(`请访问：http://localhost:${process.env.PORT ?? 8081}`)
   // 启用跨域
-  // console.log(`允许的前端域名：${process.env.REQUEST_ORIGIN}`)
   //  [process.env.REQUEST_ORIGIN ?? 'http://localhost:3000']
-  const allowedOrigins = process.env.REQUEST_ORIGIN?.split(',') || ['https://example.com']
+  const allowedOrigins = process.env.REQUEST_ORIGIN?.split(',') || ['https://nav.myltx.top']
   app.enableCors({
-    origin: allowedOrigins, // 允许的前端域名
+    origin: (origin, callback) => {
+      // 检查是否在允许列表中
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }, // 允许的前端域名
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',              // 允许的 HTTP 方法
     credentials: true,                                      // 是否允许携带凭证（如 Cookies）
   });
