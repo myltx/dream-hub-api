e99ee4938a75f7bdf982de07cd5f379d163e2ba1
myltx <summmayl@gmail.com>
2025-01-09 11:34:53 +0800

    feat(client): enhance tab navigation with smooth scrolling and improve tab layout

--- apps/client/src/pages/index.vue
+++ apps/client/src/pages/index.vue
@@ -17,6 +17,12 @@ const loading = ref(true);
 const onChangeTab = (id: number) => {
   loading.value = true;
   activeTab.value = id;
+  // 滚动到当前 Tab 的可见区域
+  const tabElement = document.querySelector(`.tab[data-id="${id}"]`);
+  console.log(tabElement, 'tabElement');
+  if (tabElement) {
+    tabElement.scrollIntoView({ behavior: 'smooth', inline: 'center' });
+  }
   getWebSites();
 };
 
@@ -57,20 +63,25 @@ onMounted(async () => {
 
 <template>
   <div class="h-100%">
-    <div class="py-2 px-2 flex items-center mt-2 mx-2 shadow-md rounded-lg">
-      <div
-        v-for="tab in categorys"
-        :key="tab.id"
-        class="p-5 cursor-pointer h-10 rounded-5 flex items-center justify-center mr-3 shadow hover:text-blue hover:font-500"
-        :class="[
-          tab.id == activeTab
-            ? 'text-blue-500 border-blue-500 font-500'
-            : 'text-gray-500 border-gray-500',
-          $colorMode.value === 'dark' ? 'bg-gray-800' : '',
-        ]"
-        @click="onChangeTab(tab.id)"
-      >
-        {{ tab.name }}
+    <div
+      class="py-2 px-2 mt-2 mx-2 shadow-md rounded-lg w-100% overflow-hidden"
+    >
+      <div class="w-99% flex items-center whitespace-nowrap overflow-x-auto">
+        <div
+          v-for="tab in categorys"
+          :key="tab.id"
+          :data-id="tab.id"
+          class="tab p-5 flex-1 cursor-pointer text-4 h-10 rounded-5 flex items-center justify-center mr-3 shadow hover:text-blue hover:font-500"
+          :class="[
+            tab.id == activeTab
+              ? 'text-blue-500 border-blue-500 font-500'
+              : 'text-gray-500 border-gray-500',
+            $colorMode.value === 'dark' ? 'bg-gray-800' : '',
+          ]"
+          @click="onChangeTab(tab.id)"
+        >
+          {{ tab.name }}
+        </div>
       </div>
     </div>
     <div class="mt-5 overflow-y-auto h-89%">
