c0625809f8d620e1dac95b0886ac57ee8428d1d5
myltx <summmayl@gmail.com>
2025-01-21 17:17:41 +0800

    feat(client): add navigation modules and visit count display in admin page

--- apps/client/src/pages/admin/index.vue
+++ apps/client/src/pages/admin/index.vue
@@ -5,8 +5,39 @@ definePageMeta({
   layout: 'admin',
 });
 const router = useRouter();
+const visitAllCount = ref(0);
+const moduleList = ref([
+  {
+    name: '站点管理',
+    icon: 'icon-park-outline:ad-product',
+    link: '/admin/website',
+    description: '配置站点信息，如站点名称、站点描述等',
+  },
+  {
+    name: '分类管理',
+    icon: 'icon-park-outline:all-application',
+    link: '/admin/categories',
+    description: '管理分类信息，如文章分类等',
+  },
+  {
+    name: '标签管理',
+    icon: 'heroicons:bookmark',
+    link: '/admin/tag',
+    description: '管理标签信息，如文章标签等',
+  },
+  {
+    name: '文件管理',
+    icon: 'heroicons:archive-box',
+    link: '/admin/file',
+  },
+]);
 
-const { data } = await getSiteAccessInterViewCount();
+const getVisitCount = () => {
+  getSiteAccessInterViewCount().then((res) => {
+    visitAllCount.value = res.data;
+  });
+};
+getVisitCount();
 
 const toLink = (link: string) => {
   router.push(link);
@@ -23,35 +54,36 @@ const toLink = (link: string) => {
     <Icon name="emojione:party-popper" class="mr-2" />
     当前项目共被访问:
     <span class="text-blue-500">
-      {{ data }}
+      {{ visitAllCount }}
     </span>
-    次 <Icon name="dashicons:update" />
+    次
+    <Icon
+      name="icon-park:reload"
+      class="cursor-pointer text-xl ml-1"
+      @click="getVisitCount"
+    />
   </div>
   <!-- 快捷导航 -->
-  <div class="text-xl font-bold mb-4">快捷导航</div>
-  <div class="grid grid-cols-2 gap-4">
-    <div
-      class="bg-white rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer"
-      @click="toLink('/admin/website')"
-    >
-      <div class="text-xl font-bold mb-2">站点管理</div>
-      <div class="text-gray-500">配置站点信息，如站点名称、站点描述等</div>
+  <div
+    class="w-30% light:bg-white dark:bg-#101726 rounded-lg p-4 shadow-md mt-4"
+  >
+    <div class="text-xl font-bold mb-4 flex items-center">
+      <Icon name="icon-park-outline:all-application" class="text-xl mr-1" />
+      快捷导航
     </div>
-    <div
-      class="bg-white rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer"
-      @click="toLink('/admin/categories')"
-    >
-      <div class="text-xl font-bold mb-2">分类管理</div>
-      <div class="text-gray-500">管理分类信息，如文章分类等</div>
-    </div>
-    <div
-      class="bg-white rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer"
-      @click="toLink('/admin/tag')"
-    >
-      <div class="text-xl font-bold mb-2">标签管理</div>
-      <div class="text-gray-500">管理标签信息，如文章标签等</div>
+    <div class="grid grid-cols-2 gap-4">
+      <div
+        class="flex flex-col items-center justify-center cursor-pointer rounded-lg p-4 border-1 cursor-pointer hover:transition-all duration-300 hover:shadow-md"
+        v-for="item in moduleList"
+        :key="item.name"
+        @click="toLink(item.link)"
+      >
+        <Icon :name="item.icon" class="text-xl mb-2" />
+        {{ item.name }}
+      </div>
     </div>
   </div>
+  <!-- 这里 添加一个显示数据的模块 -->
 </template>
 
 <style scoped></style>
