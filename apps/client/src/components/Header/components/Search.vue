ec54ed3727411974c10b2dd0dc5e7cf102eebb58
myltx <summmayl@gmail.com>
2025-01-22 11:02:35 +0800

    style(client): fix search scrolling problem

--- apps/client/src/components/Header/components/Search.vue
+++ apps/client/src/components/Header/components/Search.vue
@@ -78,31 +78,6 @@ const showModal = () => {
 const onQuery = () => {
   console.log('onQuery');
 };
-const ui = {
-  wrapper:
-    'flex flex-col flex-1 min-h-0 divide-y divide-gray-200 dark:divide-gray-700 bg-gray-50 dark:bg-gray-800',
-  container:
-    'relative flex-1 overflow-y-auto divide-y divide-gray-200 dark:divide-gray-700 scroll-py-2',
-  input: {
-    base: 'w-full h-14 px-4 placeholder-gray-400 dark:placeholder-gray-500 bg-transparent border-0 text-gray-900 dark:text-white focus:ring-0 focus:outline-none',
-  },
-  group: {
-    label: 'px-2 my-2 text-xs font-semibold text-gray-500 dark:text-gray-400',
-    command: {
-      base: 'flex justify-between select-none cursor-default items-center rounded-md px-2 py-2 gap-2 relative',
-      active: 'bg-gray-200 dark:bg-gray-700/50 text-gray-900 dark:text-white',
-      container: 'flex items-center gap-3 min-w-0',
-      icon: {
-        base: 'flex-shrink-0 w-5 h-5',
-        active: 'text-gray-900 dark:text-white',
-        inactive: 'text-gray-400 dark:text-gray-500',
-      },
-      avatar: {
-        size: '2xs',
-      },
-    },
-  },
-};
 </script>
 <template>
   <UTooltip
@@ -132,6 +107,7 @@ const ui = {
       :autoselect="false"
       :groups="websiteList"
       placeholder="请输入搜索内容"
+      class="custom-command-palette"
       :close-button="{
         icon: 'i-heroicons-x-mark-20-solid',
         color: 'gray',
@@ -152,7 +128,9 @@ const ui = {
         },
         resultLimit: 10,
       }"
-      :ui="ui"
+      :ui="{
+        container: 'custom-command-palette-container',
+      }"
       @close="isOpen = false"
       @update:model-value="onSelect"
     >
@@ -160,4 +138,8 @@ const ui = {
   </UModal>
 </template>
 
-<style scoped></style>
+<style>
+.custom-command-palette-container {
+  height: 50vh !important;
+}
+</style>
