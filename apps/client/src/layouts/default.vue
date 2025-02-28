19ee5301be13d16edad5150ef50fd81f85229e6b
myltx <summmayl@gmail.com>
2025-02-28 14:47:04 +0800

    refactor(layout): simplify default layout and index page structure

--- apps/client/src/layouts/default.vue
+++ apps/client/src/layouts/default.vue
@@ -2,11 +2,9 @@
 <template>
   <div class="h-100vh">
     <Header />
-    <div class="w-full h-90% rounded-2">
-      <div class="h-14 w-100%"></div>
-      <div class="flex h-100%">
-        <slot />
-      </div>
+    <div class="h-14 w-100%"></div>
+    <div class="w-full h-90% rounded-2 flex">
+      <slot />
     </div>
     <Footer />
   </div>
